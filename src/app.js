929fe65fed5b7f5420649bc45b2f14fe07e8956e
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2019-01-17 12:12:30 +0530

    Removed unnecessary commented code and unnecessary code

--- src/app.js
+++ src/app.js
@@ -3,25 +3,12 @@ const createApp = require("../public/framework.js");
 const app = createApp();
 const ENCODING = "utf8";
 
-
-
-// const html = {
-//   home: ()=>{}
-// };
-
-const html = {
-  homepage: (req, res, fileName) => readFile(fileName, (err, contents) => {
-    send(res, contents);
-  })
-};
-
 const send = (res, contents, statusCode = 200) => {
   res.write(contents);
   res.statusCode = statusCode;
   res.end();
 }
 const readbody = (req, res, jumpToNext) => {
-  // const contents = "";
   let contents = "";
   req.on("data", chunk => contents += chunk);
   req.on("end", () => {
@@ -53,16 +40,9 @@ const serveFile = (req, res) => {
     console.log(err);
   }
 };
-// Export a function that can act as a handler
 
 app.use(readbody);
 app.use(serveFile);
 app.use(notFound);
-
-const getURL = function(req,res){
-  if(req.url == "/"){
-    
-  }
-}
 
 module.exports = app;
